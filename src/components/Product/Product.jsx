7a719af45d04d4c120020d6939ed29f5a93243c8
SadmanJawad <jawadnabil977@gmail.com>
2023-03-28 13:08:13 +0600

    card fixed,product info added,Add to cart button added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const {img, name, price, seller, quantity} = props.product;
+    const {img, name, price, seller, quantity, ratings} = props.product;
 
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <h6>{name}</h6>
+            <div className='prodcut-info'>
+            <h6 className='product-name'>{name}</h6>
+            <p>Price: ${price}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings} Star</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
         </div>
     );
 };

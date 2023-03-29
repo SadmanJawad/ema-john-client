70b7364bef4935ea329472b21998b628c4bf20a1
SadmanJawad <jawadnabil977@gmail.com>
2023-03-29 20:08:30 +0600

    Handle quantity from storage to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,10 +9,23 @@ console.log(cart)
  
 let totalPrice = 0;
 let totalShipping = 0;
+let quantity = 0;
+
 
 for (const product of cart){
-    totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping;
+
+  //! technique for selected quantity get 0,after reload but stored in localStorage
+  // * easy technique
+  // if(product.quantity === 0){
+  //   product.quantity = 1
+  // }
+  //* shortcut technique
+  // product.quantity = product.quantity || 1;
+
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping * product.quantity;
+    quantity = quantity + product.quantity;
+
 }
 const tax = totalPrice*7/100;
 const grandTotal = totalPrice + totalShipping + tax;
@@ -21,7 +34,7 @@ const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice} </p>
       <p>Shipping: {totalShipping}</p>
       <p>Tax: ${tax}</p>
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -39,9 +39,23 @@ const Shop = () => {
 
   const handleAddToCart = (product) => {
     // cart.push(product);
-    const newCart = [...cart, product];
-    setCart(newCart);
+    // const newCart = [...cart, product];
 
+    // *hard technique ( Cart.jsx :17)
+    // if product doesn't exist in the cart , then set quantity = 1, if exist update quantity by 1
+    let newCart = [];
+    const exists = cart.find(pd => pd.id === product.id);
+    if (!exists){
+      product.quantity = 1 ;
+      newCart = [...cart, product]
+    }
+    else{
+      exists.quantity = exists.quantity +1 ;
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart = [...remaining, exists]
+    }
+
+    setCart(newCart);
     //! add cart data to LocalStorage (51-4)
     addToDb(product.id);
   };

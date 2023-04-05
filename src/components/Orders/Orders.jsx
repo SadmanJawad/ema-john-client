b3e09bc1178a972cff30b5db1089e47bb6f9f372
SadmanJawad <jawadnabil977@gmail.com>
2023-04-06 01:52:30 +0600

    order route fully updated

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Cart from "../Cart/Cart";
 import { useLoaderData } from "react-router-dom";
 import './Orders.css'
 import ReviewItem from "../ReviewItem/ReviewItem";
+import { removeFromDb } from "../../utilities/fakedb";
 
 const Orders = () => {
-    const cart = useLoaderData()
-    console.log(cart)
+    const savedCart = useLoaderData()
+    const [cart, setCart] = useState(savedCart)
+    // console.log(savedCart)
+  const handleRemoveFromCart = (id) => {
+    // console.log(id);
+    const remaining = cart.filter(product => product.id !== id)
+    setCart(remaining);
+    removeFromDb(id)
+  }
+
   return (
     <div className="shop-container">
       <div className="review-container">
         {
-          cart.map(product => <ReviewItem
+         cart.map(product => <ReviewItem
           key={product.id}
           product={product}
+          handleRemoveFromCart={handleRemoveFromCart}
           >  
           </ReviewItem>)
         }
